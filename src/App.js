3b7a6645a63e7fabff7a0cd115d81d8b418e5c58
Jake <midg3tjake@live.co.uk>
2017-10-20 22:55:10 +0100

    Server.js Test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
+const express = require('express');
+const path = require('path');
+const app = express();
 
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const app = require('./app');
+
+const express = require('express');
+const app = express();
+const options = {
+    key: fs.readFileSync('SSL/privkey1.pem'),
+    cert: fs.readFileSync('SSL/fullchain1.pem'),
+    ca: fs.readFileSync('SSL/chain1.pem')
+};
+const https = require('https');
+const Server = https.createServer(options, app);
+
+Server.listen(8080, 'precure.ddns.net');
